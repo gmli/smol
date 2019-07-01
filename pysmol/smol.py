8f5475e9e498f50cfd56464fa38dba717a638403
gmli <guillaume.ludwig@gmail.com>
2019-07-01 21:19:19 +0200

    Rendre la classe SMOL instanciable une seule fois (singleton).

--- pysmol/smol.py
+++ pysmol/smol.py
@@ -3,14 +3,7 @@
 """
 Module principal pour la console SMOL.
 
-Aucune idée de comment je souhaite mon API pour le moment. À définir.
-
-TODO : tout réécrire en fonctionnel ? Façon gamebuino ou pico8. Avec un :
-
-> import smol
-
-On aurait l'écran initialisé. Ensuite quelques fonctions pour aider et voilà.
-Quelques objects (SMOLWorld ?) pour gérer les sprites/layers/scènes/etc.
+Gère l'écran, les boutons, etc.
 
 """
 
@@ -21,12 +14,29 @@ from machine import Pin, SPI, Signal
 
 black = rgb.color565(0,0,0)
 
-spi = SPI(1, baudrate=59000000, polarity=0, phase=0, sck=Pin(14), mosi=Pin(13), miso=Pin(12)) 
-display = st7735.ST7735R(spi, dc=Pin(19), cs=Pin(18), rst=Pin(5), width=130, height=130)
+
+def singleton(class_):
+    """
+    La classe [SMOL] représente la console, elle ne doit être instanciée qu'une seule fois.
+    La fonction [singleton] est utilisée pour créer un singleton de la classe [SMOL] via decorator.
+
+    Pour référence sur les singletons : 
+    https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python/
+
+    Habituellement j'utilise les metaclass pour ça, mais ce n'est pas disponible dans MicroPyton pour 
+    le moment. Le problème avec les décorateurs c'est qu'il n'est pas possible d'utiliser des méthodes 
+    statiques. À voir s'il est possible de faire autrement à l'avenir.
+    """
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
 
 
-
-class SMOL(object):
+@singleton
+class SMOL:
 
     """
     On ne gère ici que la console SMOL, donc toutes les valeurs pour les pins sont en fixées.
@@ -41,11 +51,14 @@ class SMOL(object):
     """
 
     def __init__(self):
+
+        self.spi = SPI(1, baudrate=59000000, polarity=0, phase=0, sck=Pin(14), mosi=Pin(13), miso=Pin(12)) 
+        self.display = st7735.ST7735R(self.spi, dc=Pin(19), cs=Pin(18), rst=Pin(5), width=130, height=130)
         
         self.fb = framebuf.FrameBuffer(bytearray(130*130*2), 130, 130, framebuf.RGB565)
         self.fb.fill_rect(0, 0, 130, 130, black) # voir pour les valeurs 130 <-> 128
 
-        display.fill(black)
+        self.display.fill(black)
         # À ce moment SMOL() affiche un écran noir.
 
 
@@ -68,6 +81,6 @@ class SMOL(object):
         """
         Blit du framebuffer entier sur l'écran via SPI.
         """
-        display.blit_buffer(self.fb, 0,0, 130,130)
+        self.display.blit_buffer(self.fb, 0,0, 130,130)
             
 
