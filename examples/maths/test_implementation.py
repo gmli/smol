1fdd3a47910929a257afbeaf256c18be87ab69ca
Papa Maker <guillaume.ludwig@gmail.com>
2019-07-02 11:50:57 +0200

    Update test_implementation.py

--- examples/maths/test_implementation.py
+++ examples/maths/test_implementation.py
@@ -11,4 +11,5 @@ display.refresh()
 display.refresh() -> soit ça envoi les données via SPI immédiatement, 
 soit ça flag l'écran pour qu'un timer ait l'autorisation d'updater l'écran si x millisecondes sont passées.
 Avec par exemple un display.set_max_fps(xx) qui donne les fps _maxi_.
+Par contre avec le flag, peut-être qu'un triple buffering serait nécessaire -> tester si on a des problèmes de scintillement ou tearing.
 '''
